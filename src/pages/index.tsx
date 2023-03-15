6dcd2ec85d845a90107dcab78e31215d9c6d78a7
Nicola Erario <nicola.erario@gmail.com>
2023-03-15 20:45:48 +0100

    feat: Add example of using store and actions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,15 @@
 import { type NextPage } from 'next';
+import { signIn, signOut, useSession } from 'next-auth/react';
 import Head from 'next/head';
 import Link from 'next/link';
-import { signIn, signOut, useSession } from 'next-auth/react';
-
+import useStore from '~/store';
 import { api } from '~/utils/api';
 
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: 'from tRPC' });
+
+  const bears = useStore((state) => state.bears);
+  const addBears = useStore((state) => state.increase);
 
   return (
     <>
@@ -24,8 +27,14 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+            {bears}
           </h1>
+          <button
+            className="rounded bg-white p-4"
+            onClick={addBears}
+          >
+            Add 1 Bear
+          </button>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
